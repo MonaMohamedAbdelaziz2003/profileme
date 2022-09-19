da76c73353dc335882ef2c3d51ebb1ae352462b5
unknown <akbra2644@gmail.com>
2022-09-19 17:02:16 +0200

    monamohamed

---
+++ first.js
@@ -0,0 +1,11 @@
+var minue = document.getElementById("ul-of-nav");
+
+function mi() {
+    if (minue.style.display = "block") {
+        minue.style.display = "none";
+    }
+    if (minue.style.display = "none") {
+        minue.style.display = "block";
+    }
+
+}
